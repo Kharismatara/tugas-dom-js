a7590eadf86da343ae4ef19425db98f2236c5030
kharisma tara mukti <kharismataram@gmail.com>
2023-04-05 21:39:11 +0700

    selesai

--- script.js
+++ script.js
@@ -40,14 +40,36 @@ navbar.appendChild(a);
 let clonednavbar = navbar.cloneNode(true);
 clonednavbar.id = "navbar-layanan";
 document.body.appendChild(clonednavbar);
-//section 5
+//section 5-6
 document.getElementById("penjelasan").setAttribute("class", "header");
-
-// Menambahkan atribut style ke elemen dengan id "paragraf"
 document.getElementById("paragraf").setAttribute("style", "color: red; font-size: 20px;");
 
 function removeStyle() {
     let para = document.getElementById("myParagraph");
     para.removeAttribute("style");
     para.classList.remove("highlight");
+}
+div.style.backgroundColor = "blue";
+div.style.color = "white";
+div.style.padding = "20px";
+
+//section 7-8
+function submitForm() {
+    // mengambil nilai dari input form
+    var nama = document.getElementById("nama").value;
+    var email = document.getElementById("email").value;
+
+    // melakukan validasi input
+    if (nama == "") {
+        alert("Nama tidak boleh kosong");
+    } else if (email == "") {
+        alert("Email tidak boleh kosong");
+    } else if (!email.includes("@")) {
+        alert("Email tidak valid");
+    } else {
+        // melakukan proses submit form ke server
+        alert("Form berhasil disubmit dengan nilai: " + nama + ", " + email);
+        document.getElementById("nama").value = "";
+        document.getElementById("email").value = "";
+    }
 }
